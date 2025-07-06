b542008e38cd5809981a6bcb62c4b5c8e8ca0303
filiptrojandatasentics <filip.trojan@datasentics.cz>
2025-07-06 16:52:45 +0200

    fixed update_user_status.sql

--- sql_templates/update_user_status.sql
+++ sql_templates/update_user_status.sql
@@ -1,10 +1,10 @@
-UPDATE {{ table_name }}
-                SET 
-                    status = '{{ new_status }}',
-                    updated_at = CURRENT_TIMESTAMP
-                WHERE user_id IN (
-                    {% for user_id in user_ids %}
-                        {{ user_id }}
-                        {%- if not loop.last -%},{%- endif -%}
-                    {% endfor %}
-                );
+update {{ table_name }}
+set
+    status = '{{ new_status }}'
+    , updated_at = current_timestamp
+where user_id in (
+    {% for user_id in user_ids %}
+    {{ user_id }}
+    {%- if not loop.last -%},{%- endif -%}
+    {% endfor %}
+);
